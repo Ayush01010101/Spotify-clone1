3819089adf7eda6ff9718c8e2188f1468593202b
Ayush01010101 <ayushawachar0989@gmail.com>
2024-07-02 16:30:29 +0530

    changes

--- script.js
+++ script.js
@@ -1,57 +1,70 @@
+let currentMusic=new Audio();
 async function getsongs() {
-    let songs = await fetch("http://127.0.0.1:3000/songs/");
-    let song_data = await songs.text();
-    let div = document.createElement("div");
-    div.innerHTML = song_data;
-    // console.log(song_data_div.innerHTML=song_data)
-    let song_arr = div.getElementsByTagName("a");
-    let newarr=[];
-    let arr = [];
-    for (let i = 0; i < song_arr.length; i++) {
-        if (song_arr[i].href.endsWith(".mp3")) {
-            let newarr=[song_arr[i].href.split("/songs/")[1]];
-            
-            let string=newarr.join("");
-            
-            let p1=string.replaceAll("%20"," ");
-            let p2=p1.replaceAll(".mp3","");
-            arr.push(p2)
+  let songs = await fetch("http://127.0.0.1:3000/songs/");
+  let song_data = await songs.text();
+  let div = document.createElement("div");
+  div.innerHTML = song_data;
+  // console.log(song_data_div.innerHTML=song_data)
+  let song_arr = div.getElementsByTagName("a");
+  let newarr = [];
+  let arr = [];
+  for (let i = 0; i < song_arr.length; i++) {
+    if (song_arr[i].href.endsWith(".mp3")) {
+      let newarr = [song_arr[i].href.split("/songs/")[1]];
 
+      let string = newarr.join("");
 
-            
-            
-        }
-        
+      let p1 = string.replaceAll("%20", " ");
+      let p2 = p1.replaceAll(".mp3", "");
+      arr.push(p2);
     }
-    
-    // console.log(arr)
-    return arr;
+  }
+
+  // console.log(arr)
+  return arr;
 }
+const play_song=((track)=>{
+  currentMusic.src=`Songs\\${track}.mp3`;
+  currentMusic.play();
+})
 
 async function main() {
-    let get = await getsongs();
-    
-    console.log(get)
-   
-    // let song = new Audio(get[4]);   
+  let get = await getsongs();
+  // let plays=new Audio("Songs\\DNA .mp3");
+  // plays.play();
 
-   for (const i of get) {
-        let song=document.getElementById("song-list");
+  // let song = new Audio(get[4]);
 
-        song.innerHTML=song.innerHTML+`<div class="songcard">
+
+  for (const i of get) {
+    let song = document.getElementById("song-list");
+    song.innerHTML =
+      song.innerHTML +
+      `<div class="songcard">
                         <img class="invert songcard_img1" src="controls/music.svg" alt="music logo">
                         <div class="songinfo">
-                            <div class="song__name">
+                            <div class="song__name" id="song__name">
                                 ${i}
                             </div>
                         </div>
                         <img class="songcard_img2" src="controls/play.svg" alt="">
-                    </div>`
+                        
+                    </div>`;
 
-   }
 
+  }
+  for (let i = 0; i < get.length; i++) {
+    let test = document.querySelector(".song-list").children[i];
+    test.addEventListener("click", () => {
+      let track = test.innerText;
+      
+      play_song(track);
+    
+      
+      
+     
+    });
+  }
 }
 
 main();
-
-
---
+++ test.js
@@ -0,0 +1,3 @@
+let string = "h  l   lo";
+let hello = string.trim();
+console.log(hello);
